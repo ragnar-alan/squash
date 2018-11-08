5ffbad3b847776c1457ff0fe2d4a37bcea6d5421
Boros Tamás <boros.tamas456@gmail.com>
2018-11-08 23:45:43 +0100

    google verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="google-site-verification" content="_-8_pAIjS96MLKrs1XrklwKijngkC_yqiq5CrXQ5rCw" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
